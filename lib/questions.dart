e5018bf33813731b0ccb8a431b63fd7724dda356
kamyarmg <kmgurmia@gmail.com>
2025-08-28 01:48:12 +0330

    fix: Standardize answer formatting in questions for consistency

--- lib/questions.dart
+++ lib/questions.dart
@@ -1,23 +1,22 @@
 final Map<String, String> qA = {
   // Human rights
   "The _ _ Cylinder, from ancient Persia, is called the first charter of human rights, declaring freedom of religion and abolition of slavery in conquered lands":
-      "Cyrus",
+      "CYRUS",
   "The Universal Declaration of _ _ Rights (UDHR) was adopted on December 10, 1948, by the United Nations":
-      "Human",
-  "Right to _ _ is considered the foundation of all other human rights": "life",
-  "_ _ from torture is one of the only rights considered absolute under international law meaning it cannot be restricted under any circumstance":
-      "Freedom",
+      "HUMAN",
+  "Right to _ _ is considered the foundation of all other human rights": "LIFE",
   "_ _ is prohibited universally, but modern form (human trafficking, forced labor) still affects an estimated 50 million people worldwide":
-      "Slavery",
+      "SLAVERY",
   "Women have the same rights as _ _ in every area of life. No one can treat them unfairly because of their gender":
-      "men",
+      "MEN",
   "Women have the right to go to school, college, or university, _ _ helps them become independent and make their own decisions":
-      "education",
+      "EDUCATION",
   "Women can work in any job or profession they choose. They should receive equal _ _ and safe working conditions":
-      "pay",
-  "Everyone can express their opinions _ _ without fear. They have the right to participate in discussions, protests, or media":
-      "freely",
+      "PAY",
   "Women should have the same chances as _ _ in education, work, politics, and sports. Society should remove all barriers that stop women from achieving their goals":
-      "men",
-      "All people are _ _, no matter their race, gender, religion, or status. Everyone should be treated fairly.": "equal",
+      "MEN",
+  "All people are _ _, no matter their race, gender, religion, or status. Everyone should be treated fairly":
+      "EQUAL",
+  "Everyone has the right to _ _ and start a family. They should make these choices freely":
+      "MARRY",
 };
